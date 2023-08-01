b15f8fc27b3ff4a728a8f641318e332413ab7e23
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-08-01 12:43:28 +0000

    j

---
+++ .devcontainer/loops/while.js
@@ -0,0 +1,17 @@
+//while loop :- first the condition will be checked then the statement will be executed
+// while (true) {
+//     console.log("jhello");
+// }
+// let mySuperHero=["BATMAN","SUPERMAN","FLASH"]
+// let arr=0
+// while(arr<mySuperHero.length){
+//     console.log(mySuperHero[arr]);
+//     arr++;
+// }
+
+//do while loop :- first it will get executed and then the condition will be checked
+let score=1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=11);
